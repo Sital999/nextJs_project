479cb16d786905e1057646e511795cc7e5635ac9
sital999 <sitalnagarkoti123@gmail.com>
2023-02-01 21:13:36 +0545

    first commit

---
+++ quiz/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">Welcome To Quiz</h1>
+    </>
+  ); 
+}
+
+export default Home
